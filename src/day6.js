ff24541211d4431683453b5c14681fe4fde85d44
U-INTERN\aser <aser@SJV00197.intern.jordbruksverket.se>
2015-12-07 12:41:22 +0100

    Solution for day 6 part 2

--- src/day6.js
+++ src/day6.js
@@ -1,43 +1,66 @@
-exports.howManyLights = function(input) {
-  var lights = {};
-  for(var i = 0; i < input.length; i++) {
-    if(input[i].match(/turn on/)) {
-      fixTheLights(lights, input[i], _turnOnLights);
-    } else if(input[i].match(/turn off/)) {
-      fixTheLights(lights, input[i], _turnOffLights);
-    } else if(input[i].match(/toggle/)) {
-      fixTheLights(lights, input[i], _toggleLights);
+module.exports = {
+    howManyLights: function(input) {
+      var lights = parseInput(input, {on: _turnOnLights, off: _turnOffLights, toggle: _toggleLights});
+      return Object.getOwnPropertyNames(lights).length;
+    },
+    howBright: function(input) {
+      var lights = parseInput(input, {on: _turnOnLights, off:_decreasLights, toggle: _increaseBrightness});
+      return Object.keys(lights).reduce(function(previous, key) {
+        return previous + lights[key];
+      }, 0);
     }
-  }
-  return Object.getOwnPropertyNames(lights).length;
-};
 
-function fixTheLights(lights, commando, f) {
-  var match = commando.match(/(\d+),(\d+)/g);
-  var startMatcher = match[0].split(','); 
-  var endMatcher = match[1].split(','); 
-  var start = {x:parseInt(startMatcher[0]), y:parseInt(startMatcher[1])};
-  var end = {x:parseInt(endMatcher[0]), y:parseInt(endMatcher[1])};
-  
-  for(var x = start.x; x <= end.x; x++) {
-    for(var y = start.y; y <= end.y; y++) {
-      f(lights, x+':'+y);
+  };
+
+  function parseInput(input, functions) {
+    var lights = {};
+    for(var i = 0; i < input.length; i++) {
+      if(input[i].match(/turn on/)) {
+        fixTheLights(lights, input[i], functions.on);
+      } else if(input[i].match(/turn off/)) {
+        fixTheLights(lights, input[i], functions.off);
+      } else if(input[i].match(/toggle/)) {
+        fixTheLights(lights, input[i], functions.toggle);
+      }
     }
+    return lights;
   }
-}
 
-function _turnOffLights(lights, key) {
-  delete lights[key];
-}
+  function fixTheLights(lights, commando, f) {
+    var match = commando.match(/(\d+),(\d+).*?(\d+),(\d+)/).map(Number);
+    var start = {x:match[1], y:match[2]};
+    var end = {x:match[3], y:match[4]};
 
-function _turnOnLights(lights, key) {
-  lights[key] = 1; 
-}
+    for(var x = start.x; x <= end.x; x++) {
+      for(var y = start.y; y <= end.y; y++) {
+        f(lights, x+':'+y);
+      }
+    }
+    }
 
-function _toggleLights(lights, key) {
-  if(lights[key] === 1) {
-    _turnOffLights(lights, key);
-  } else {
-    _turnOnLights(lights, key);
-  }
-}
+    function _turnOffLights(lights, key) {
+        delete lights[key];
+    }
+
+    function _decreasLights(lights, key) {
+      lights[key] = lights[key] -1 || 0;
+      if(lights[key] === 0) {
+        delete lights[key];
+      }
+    }
+    function _turnOnLights(lights, key) {
+      lights[key] = lights[key] +1 || 1; 
+    }
+    
+    function _increaseBrightness(lights, key) {
+      lights[key] = lights[key] + 2 || 2; 
+    }
+
+
+    function _toggleLights(lights, key) {
+      if(lights[key] > 0) {
+        _turnOffLights(lights, key);
+      } else {
+        _turnOnLights(lights, key);
+      }
+    }
--- test/day6_test.js
+++ test/day6_test.js
@@ -16,7 +16,24 @@ describe('Santas light stuff', function() {
 
   it('turn on first row then toggle a 3 by three should result in 1003', function() {
     expect(day6.howManyLights(['turn on 0,0 through 0,999', 'toggle 0,0 through 2,2'])).to.equal(1003);
-    
+  });
+
+  it('turn on 0,0 through 0,0 lights the first light, brightnes 1', function() {
+    expect(day6.howBright(['turn on 0,0 through 0,0'])).to.equal(1);
+  });
+
+  it('turn on 0,0 through 999,0 lights the first line of light, brightness 1000', function() {
+    expect(day6.howBright(['turn on 0,0 through 999,0'])).to.equal(1000);
+  });
+  it('turn on then of resuls in zero, brightness 0', function() {
+    expect(day6.howBright(['turn on 0,0 through 0,0', 'turn off 0,0 through 0,0'])).to.equal(0);
+
+  });
+  it('turn on first row then toggle a 3 by three, brightness 1018', function() {
+    expect(day6.howBright(['turn on 0,0 through 0,999', 'toggle 0,0 through 2,2'])).to.equal(1018);
+  });
+  it('toggle 0,0 through 999,999', function() {
+    expect(day6.howBright(['toggle 0,0 through 999,999'])).to.equal(2000000);
   });
 });
 
