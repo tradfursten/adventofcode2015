fa7f4218e68f01388801c0514838a16d3c2f5518
tradfursten <adam@sernheim.com>
2015-12-10 14:01:07 +0100

    Update forth_test.js
    Enable test for day 4

--- test/forth_test.js
+++ test/forth_test.js
@@ -2,22 +2,22 @@ var expect = require('chai').expect;
 var forth = require('../src/forth.js');
 
 describe('Test suite for day 4', function() {
-  xit('abcdef returns 609043', function() {
+  it('abcdef returns 609043', function() {
     this.timeout(15000);
     expect(forth.getAdventCoin('abcdef', 5)).to.equal(609043);
   });
 
-  xit('pqrstuv returns 1048970', function(){
+  it('pqrstuv returns 1048970', function(){
     this.timeout(15000);
     expect(forth.getAdventCoin('pqrstuv', 5)).to.equal(1048970);
   });
 
-  xit('problem input? iwrupvqb', function() {
+  it('problem input? iwrupvqb', function() {
     this.timeout(15000);
     console.log(forth.getAdventCoin('iwrupvqb', 5));
   });
 
-  xit('puzzelinput with 6 zeroes', function() {
+  it('puzzelinput with 6 zeroes', function() {
     this.timeout(600000);
     console.log(forth.getAdventCoin('iwrupvqb', 6));
   });
