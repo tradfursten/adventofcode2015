713975ca616d37110d7dd0fc25c611a5300d2f7b
Adam Sernheim <adam@sernheim.com>
2015-12-17 23:52:01 +0100

    Started on day 13

---
+++ src/day13.js
@@ -0,0 +1,52 @@
+var permutate = require('./permutate.js');
+
+module.exports = {
+  createGraph: function(input) {
+    var o = {};
+    var match;
+    for(var i = 0; i < input.length; i++) {
+      match = input[i].match(/(\w+)/g);
+      if(!o[match[0]]) {
+        o[match[0]] = {};
+      }
+      if(!o[match[10]]) {
+        o[match[10]] = {};
+      }
+      if(match[2] ==='gain'){ 
+      o[match[0]][match[10]] = parseInt(match[3]);
+      o[match[10]][match[0]] = parseInt(match[3]);
+			} else {
+      o[match[0]][match[10]] = parseInt(match[3]);
+      o[match[10]][match[0]] = parseInt(match[3]);
+			}
+    }
+    return o;
+  },
+  shortestPath: function(graph) {
+		return _calculate(graph, Math.min, Number.MAX_SAFE_INTEGER);;
+  },
+  longestPath: function(graph) {
+  	return _calculate(graph, Math.max, Number.MIN_SAFE_INTEGER);
+  }
+}
+
+function _calculate(graph, f, initialValue) {
+  	var nodes = Object.keys(graph);
+
+  	var permutations = permutate(nodes);
+		var l =  permutations.reduce(function(prev, current) { 
+			return f(prev, _getLength(current, graph)); 
+		}, initialValue);
+
+		return l;
+}
+
+function _getLength(path, graph) {
+	var l = 0;
+	var temp;
+	for(var i = 0; i < path.length-1; i++) {
+		temp = graph[path[i]];
+		l += temp[path[i+1]];	
+	}
+	return l;
+}
